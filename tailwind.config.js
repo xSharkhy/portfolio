858fd1d0825fd4f82d2332dc34cce2287433b249
Ismael Morejón Blasco <93212283+xSharkhy@users.noreply.github.com>
2024-01-10 13:27:19 +0100

    feat: add custom themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 
-import defaultTheme from 'tailwindcss/defaultTheme'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Geist Variable"', 'Geist', ...defaultTheme.fontFamily.sans]
+        sans: ['"Geist Variable"', 'Geist', ...fontFamily.sans]
       }
     }
   },
-  plugins: [require('daisyui')]
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/theming/themes').light,
+          primary: '#26997B',
+          'primary-content': '#c7ffff',
+          secondary: '#467864',
+          'base-100': '#DFF0E6',
+          'base-200': '#d5e6dc',
+          'base-300': '#adbdb4',
+          neutral: '#333',
+          'neutral-content': '#5c5c5c'
+        },
+        dark: {
+          ...require('daisyui/src/theming/themes').dark,
+          primary: '#26997B',
+          'primary-content': '#cfffff',
+          secondary: '#345e37',
+          'base-100': '#1E1E1E',
+          'base-200': '#2d2d2d',
+          'base-300': '#454545',
+          neutral: '#FFFFFF',
+          'neutral-content': '#e0e0e0'
+        }
+      }
+    ]
+  }
 }
