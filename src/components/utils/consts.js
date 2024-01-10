0bff9cb186cae94470207bddb38493459355b3df
Ismael Morejón Blasco <93212283+xSharkhy@users.noreply.github.com>
2024-01-10 14:04:46 +0100

    feat: add experience page with timelines

---
+++ src/components/utils/consts.js
@@ -0,0 +1,23 @@
+export const EXPERIENCES = [
+  {
+    date: 'Marzo 2022',
+    title: 'Desarrollador Web - La Mina Estudio',
+    description:
+      'Desarrollo de herramientas de scraping y crawlers para la obtención de datos de sitios web. Optimización de procesos de obtención de datos y automatización de tareas. Implementación de Bases de Datos con grandes cantidades de entradas, SQL y NoSQL.'
+  }
+]
+
+export const EDUCATION = [
+  {
+    date: 'Actualmente...',
+    title: 'Máster en Creación de Videojuegos y Realidad Virtual - IES Abastos',
+    description:
+      'Cursando Desarrollo de Videojuegos para profundizar conocimientos en el desarrollo de software  y esperiencias interactivas.'
+  },
+  {
+    date: '2021 - 2023',
+    title: 'Técnico Superior en Desarrollo de Aplicaciones Web - IES Serpis',
+    description:
+      'Experiencia en tecnologías de desarrollo web como HTML5, CSS3, JavaScript, PHP 8, MySQL, Laravel, Stack MERN, etc.'
+  }
+]
