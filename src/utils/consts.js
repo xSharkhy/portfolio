504ed305d727483c5e12fc7336b0600550bb1364
Ismael Morejón Blasco <93212283+xSharkhy@users.noreply.github.com>
2024-02-06 13:19:04 +0100

    chore: add resume schema as consts

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -2,6 +2,143 @@ export const SITE_TITLE = 'Ismael Morejón - Desarrollador y programador de Apli
 export const SITE_DESCRIPTION = 'Ismael Morejón, un desarrollador profesional de aplicaciones web y videojuegos, ofrezco servicios de desarrollo de aplicaciones de alta calidad. Explora mi portafolio y ponte en contacto para tu próximo proyecto.'
 export const TRANSITION_API = true
 
+export const BASICS_ES = {
+  name: 'Ismael Morejón',
+  title: 'Desarrollador Web y de Videojuegos',
+  image: '/profile.webp',
+  email: 'ismamoreblas@gmail.com',
+  phone: null,
+  url: 'https://ismobla.dev',
+  summary: 'Desarrollador Web Júnior con gran motivación por aprender y contribuir a nuevos proyectos. Aunque mi experiencia laboral es breve, estoy dispuesto a afrontar nuevos retos y aportar valor a tu equipo. Soy una persona autodidacta con gran capacidad de aprendizaje y una fuerte motivación por convertirme en un profesional del desarrollo web.',
+  location: {
+    address: null,
+    postalCode: '46290',
+    city: 'Alcàsser',
+    countryCode: 'ES',
+    region: 'València'
+  },
+  profiles: [
+    {
+      network: 'LinkedIn',
+      username: 'ismobla',
+      url: 'https://www.linkedin.com/in/ismobla/'
+    },
+    {
+      network: 'GitHub',
+      username: 'xSharkhy',
+      url: 'https://www.github.com/xSharkhy'
+    }
+  ]
+}
+
+export const EXPERIENCES_ES = [
+  {
+    name: 'La Mina Estudio',
+    position: 'Desarrollador Web',
+    url: 'https://www.laminaestudio.com',
+    date: '2023',
+    summary:
+      'Desarrollo de herramientas de scraping y crawlers para la obtención de datos de sitios web. Optimización de procesos de obtención de datos y automatización de tareas. Implementación de Bases de Datos con grandes cantidades de entradas, SQL y NoSQL.',
+    highlights: []
+  }
+]
+
+export const VOLUNTEER_ES = [
+  {
+    organization: 'Juniors MD Alcàsser',
+    position: 'Monitor y Educador',
+    url: null,
+    startDate: '2018',
+    endDate: '2019',
+    summary:
+      'Monitor y educador en actividades de tiempo libre para niños y niñas de 9 a 17 años. Organización de campamentos, actividades y talleres, dinamización de juegos y actividades deportivas.'
+  }
+]
+
+export const EDUCATION_ES = [
+  {
+    institution: 'IES Abastos',
+    area: 'Desarrollo de Videojuegos',
+    studyType: 'Máster',
+    startDate: '2023',
+    endDate: 'Actualidad',
+    gpa: null,
+    courses: null
+  },
+  {
+    institution: 'IES Serpis',
+    area: 'Desarrollo de Aplicaciones Web',
+    studyType: 'Técnico Superior',
+    startDate: '2021',
+    endDate: '2023',
+    gpa: 8,
+    courses: null
+  },
+  {
+    institution: 'Florida Secundaria',
+    area: 'Postobligatoria',
+    studyType: 'Bachillerato Científico-Tecnológico',
+    startDate: '2018',
+    endDate: '2020',
+    gpa: 7,
+    courses: null
+  }
+]
+
+export const AWARDS_ES = [
+  {
+    title: 'PrograMaMe 2021',
+    date: '2021',
+    awarder: 'Universidad Complutense de Madrid',
+    summary: '12º clasificado en la fase nacional del concurso de programación PrograMaMe 2021.'
+  }
+]
+
+export const SKILLS = [
+  {
+    name: 'HTML5 & CSS3',
+    level: 'Avanzado'
+  },
+  {
+    name: 'JavaScript',
+    level: 'Avanzado'
+  },
+  {
+    name: 'Git',
+    level: 'Avanzado'
+  },
+  {
+    name: 'GitHub',
+    level: 'Avanzado'
+  },
+  {
+    name: 'Tailwind CSS',
+    level: 'Intermedio'
+  },
+  {
+    name: 'Astro JS',
+    level: 'Intermedio'
+  },
+  {
+    name: 'React',
+    level: 'Intermedio'
+  },
+  {
+    name: 'Node.js',
+    level: 'Intermedio'
+  },
+  {
+    name: 'MongoDB',
+    level: 'Intermedio'
+  },
+  {
+    name: 'SCRUM',
+    level: 'Intermedio'
+  }
+]
+
+// TODO: Remove and refactor below
+
 export const EXPERIENCES = [
   {
     date: 'Marzo 2023',
