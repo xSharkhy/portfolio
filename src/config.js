91483abdad93ce1fd8c4cb8fc1aa8454370c5f07
Ismael Morejón Blasco <93212283+xSharkhy@users.noreply.github.com>
2024-02-06 12:21:44 +0100

    chore: refactor project

--- src/config.js
+++
@@ -1,3 +0,0 @@
-export const SITE_TITLE = 'Ismael Morejón - Desarrollador y programador de Aplicaciones Web y Videojuegos.'
-export const SITE_DESCRIPTION = 'Ismael Morejón, un desarrollador profesional de aplicaciones web y videojuegos, ofrezco servicios de desarrollo de aplicaciones de alta calidad. Explora mi portafolio y ponte en contacto para tu próximo proyecto.'
-export const TRANSITION_API = true
